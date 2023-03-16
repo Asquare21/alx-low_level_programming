83252382d8a0629b8865baf66ef797c93b49ee38
Asquare21 <amusananuoluwa@Qgmail.com>
2023-03-16 10:37:46 -0700

    hello w

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function of this program
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle);
+	return (0);
+
+}
