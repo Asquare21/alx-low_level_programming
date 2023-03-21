8b8be305d5c93ff1e4dbdcd6f30013b7c4d7c8ad
Asquare21 <amusananuoluwa@Qgmail.com>
2023-03-21 15:48:02 -0700

    lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - it range lower case
+ *
+ * @c: parameter tp print
+ * Return: 1 if it lower case
+ * and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+
+		return (0);
+}
