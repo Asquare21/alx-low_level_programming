e236c14c001e4f29600a8ba7f5a2c5e1f446c858
Asquare21 <amusananuoluwa@Qgmail.com>
2023-03-28 03:28:02 -0700

    stropy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - copy function
+ * @src: function parameter
+ * @dest: copied function
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	if (dest == NULL)
+		return (NULL);
+	for (i = 0 ; src[i] != '\0' ; i++)
+		dest[i] = src[i];
+	return (dest);
+}
