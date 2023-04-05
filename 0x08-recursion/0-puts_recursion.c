48b65832b60f7f7678564576d47bdc9387733a71
Asquare21 <amusananuoluwa@Qgmail.com>
2023-04-05 03:57:18 -0700

    recursion string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - print recursion follow by new line
+ * @s: character input
+ * Return: always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_put_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
