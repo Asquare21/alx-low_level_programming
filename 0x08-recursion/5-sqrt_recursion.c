e532219c0604cc7ac02398c47d2237aed315bbba
Asquare21 <amusananuoluwa@Qgmail.com>
2023-04-05 10:56:22 -0700

    square root of a num in recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,7 +17,7 @@ int _sqrt_recursion(int n)
 /**
  * actual_sqrt_recursion - recurses to find the natural
  * square root of a number
- * @n: number to calc the square root 
+ * @n: number to calc the square root
  * @i: iterator
  * Return: the resulting square root
  */
